b322c55a16bbe5be93d1ff5301645b172f0c3ef7
kristijan822 <kristijanjovanovic21@gmail.com>
2026-01-16 17:37:37 +0100

    add umbrella icon to summer vacation radar and update styles

--- apps/web/src/components/tripOrganisation/sonarAnimation/sonar.jsx
+++ apps/web/src/components/tripOrganisation/sonarAnimation/sonar.jsx
@@ -1,9 +1,12 @@
 import './styles.scss';
 import React, { useState, useEffect } from "react";
-import sonarPath from "../../../images/destinations/radar/mountain-svgrepo-com.svg";
+import mountainPath from "../../../images/destinations/radar/mountain-svgrepo-com.svg";
 import waterfallPath from "../../../images/destinations/radar/waterfall-svgrepo-com.svg";
 import skiingPath from "../../../images/destinations/radar/skiing.svg";
 import snowingPath from "../../../images/destinations/radar/snowing-svgrepo-com.svg";
+import summerPath from "../../../images/destinations/radar/summer-cocktail-svgrepo-com.svg";
+import forestPath from "../../../images/destinations/radar/circle-forest-svgrepo-com.svg";
+import umbrellaPath from "../../../images/destinations/radar/summer-svgrepo-com.svg";
 
 const getCookie = (name) => {
     const value = `; ${document.cookie}`;
@@ -37,40 +40,60 @@ const RadarScan = () => {
                     <div className="rotary"></div>
 
                     <div className="display">
-                        <div className="ship-1"></div>
-                        <div className="ship-2"></div>
-                        <div className="ship-3"></div>
+
 
                         {/* MOUNTAINS */}
                         {tag === "mountains" && (
                             <>
                                 <div className="peak">
-                                    <img src={sonarPath} alt="mountain" />
+                                    <img src={mountainPath} alt="mountain" />
                                 </div>
 
                                 <div className="waterfall">
                                     <img src={waterfallPath} alt="waterfall" />
                                 </div>
+
+                                <div className="forest">
+                                    <img src={forestPath} alt="forest" />
+                                </div>
                             </>
                         )}
+
+
 
                         {/* SKIING */}
                         {tag === "skiing" && (
                             <>
-                                {/* ISTA PLANINA */}
                                 <div className="peak">
-                                    <img src={sonarPath} alt="mountain" />
+                                    <img src={mountainPath} alt="mountain" />
                                 </div>
-
                                 <div className="skiing">
                                     <img src={skiingPath} alt="skiing" />
                                 </div>
-
                                 <div className="snowing">
                                     <img src={snowingPath} alt="snowing" />
                                 </div>
                             </>
                         )}
+
+                       {tag === "summer_vacation" && (
+                           <>
+                               <div className="summer">
+                                   <img src={summerPath} alt="summer" />
+                               </div>
+
+                               <div className="umbrella">
+                                   <img src={umbrellaPath} alt="umbrella" />
+                               </div>
+                           </>
+                       )}
+
+
+
+
+
+
+
 
                     </div>
 
